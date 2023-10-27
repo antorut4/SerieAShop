030b823273b944e95b862887e88ee62555916ff2
Guerriero17 <micheleguerriero787@gmail.com>
2023-10-27 13:50:26 +0200

    db modificato

--- db.sql
+++ db.sql
@@ -1,19 +1,16 @@
--- Table structure for table `carrello`
---
 
 DROP DATABASE SerieAShop;
 CREATE DATABASE IF NOT EXISTS SerieAShop;
 USE SerieAShop;
 
 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `squadra` (
                            `idSquadra` varchar(25) NOT NULL,
                            `pathLogo` varchar(60) DEFAULT NULL,
                            PRIMARY KEY (`idSquadra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Dumping data for table `squadra`
@@ -26,8 +23,7 @@ INSERT INTO `squadra` VALUES ('Atalanta','/image/stemmi/atalanta.png'),('Bologna
 UNLOCK TABLES;
 
 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `utente` (
                           `Username` varchar(25) NOT NULL,
                           `Nome` varchar(45) DEFAULT NULL,
@@ -39,7 +35,6 @@ CREATE TABLE `utente` (
                           `admin` tinyint(1) DEFAULT NULL,
                           PRIMARY KEY (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `utente`
@@ -50,40 +45,37 @@ LOCK TABLES `utente` WRITE;
 INSERT INTO `utente` VALUES ('Fozza Napoli','admin','admin','admin@gmail.com','3456789042','via 1','admin',1);
 /*!40000 ALTER TABLE `utente` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 
 CREATE TABLE `carrello` (
-  `idCarrello` int NOT NULL auto_increment,
-  `username` varchar(25) DEFAULT NULL,
-  `totale` double DEFAULT NULL,
-  PRIMARY KEY (`idCarrello`),
-  KEY `username` (`username`),
-  CONSTRAINT `carrello_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`Username`)
+                            `idCarrello` int NOT NULL auto_increment,
+                            `username` varchar(25) DEFAULT NULL,
+                            `totale` double DEFAULT NULL,
+                            PRIMARY KEY (`idCarrello`),
+                            KEY `username` (`username`),
+                            CONSTRAINT `carrello_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ordine` (
-  `idOrdine` int NOT NULL,
-  `PrezzoTotale` double DEFAULT NULL,
-  `dataOrdine` date DEFAULT NULL,
-  `metodoDiPagamento` varchar(45) DEFAULT NULL,
-  `indirizzoSpedizione` varchar(60) DEFAULT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `idCarrello` int DEFAULT NULL,
-  PRIMARY KEY (`idOrdine`),
-  KEY `username` (`username`),
-  KEY `idCarrello` (`idCarrello`),
-  CONSTRAINT `ordine_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`Username`),
-  CONSTRAINT `ordine_ibfk_2` FOREIGN KEY (`idCarrello`) REFERENCES `carrello` (`idCarrello`)
+                          `idOrdine` int NOT NULL,
+                          `PrezzoTotale` double DEFAULT NULL,
+                          `dataOrdine` date DEFAULT NULL,
+                          `metodoDiPagamento` varchar(45) DEFAULT NULL,
+                          `indirizzoSpedizione` varchar(60) DEFAULT NULL,
+                          `username` varchar(45) DEFAULT NULL,
+                          `idCarrello` int DEFAULT NULL,
+                          PRIMARY KEY (`idOrdine`),
+                          KEY `username` (`username`),
+                          KEY `idCarrello` (`idCarrello`),
+                          CONSTRAINT `ordine_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`Username`),
+                          CONSTRAINT `ordine_ibfk_2` FOREIGN KEY (`idCarrello`) REFERENCES `carrello` (`idCarrello`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
+
 CREATE TABLE `prodotto` (
                             `idProdotto` int NOT NULL AUTO_INCREMENT,
                             `descrizione` varchar(3000) DEFAULT NULL,
@@ -110,30 +102,19 @@ UNLOCK TABLES;
 
 
 
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prodotticarrello` (
-  `idProdCarr` int NOT NULL,
-  `quantita` int DEFAULT NULL,
-  `idCarrello` int DEFAULT NULL,
-  `idProdotto` int DEFAULT NULL,
-  PRIMARY KEY (`idProdCarr`),
-  KEY `idProdotto` (`idProdotto`),
-  KEY `idCarrello` (`idCarrello`),
-  CONSTRAINT `prodotticarrello_ibfk_1` FOREIGN KEY (`idProdotto`) REFERENCES `prodotto` (`idProdotto`),
-  CONSTRAINT `prodotticarrello_ibfk_2` FOREIGN KEY (`idCarrello`) REFERENCES `carrello` (`idCarrello`)
+                                    `idProdCarr` int NOT NULL auto_increment,
+                                    `quantita` int DEFAULT NULL,
+                                    `idCarrello` int DEFAULT NULL,
+                                    `idProdotto` int DEFAULT NULL,
+                                    taglia varchar(3) default NULL,
+                                    PRIMARY KEY (`idProdCarr`),
+                                    KEY `idProdotto` (`idProdotto`),
+                                    KEY `idCarrello` (`idCarrello`),
+                                    CONSTRAINT `prodotticarrello_ibfk_1` FOREIGN KEY (`idProdotto`) REFERENCES `prodotto` (`idProdotto`),
+                                    CONSTRAINT `prodotticarrello_ibfk_2` FOREIGN KEY (`idCarrello`) REFERENCES `carrello` (`idCarrello`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
 
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-23 17:36:58
