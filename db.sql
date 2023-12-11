e087c3350aa2ebf8103e5767a82e96fa67195bcc
Domenico <domenicoarcamone@gmail.com>
2023-12-11 14:44:20 +0100

    Test UserDAO

--- db.sql
+++ db.sql
@@ -12,6 +12,7 @@ CREATE TABLE `squadra` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+
 --
 -- Dumping data for table `squadra`
 --
---
+++ src/main/java/test/UserDAOTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import model.User;
+import model.UserDAO;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDAOTest {
+
+    @Test
+    public void testDoRetrieveByUsername() throws SQLException {
+        // Assuming you have a working UserDAO class
+        UserDAO userDAO = new UserDAO();
+
+        // Insert a test user into the database (make sure the user with this username exists)
+        User testUser = new User();
+        testUser.setUsername("testUser");
+        testUser.setNome("Test");
+        testUser.setCognome("User");
+        testUser.setEmail("testuser@example.com");
+        testUser.setTelefono("1234567890");
+        testUser.setIndirizzo("Test Address");
+        testUser.setPassword("testpassword");
+        testUser.setAdmin(false);
+        userDAO.doSave(testUser);
+
+        try {
+            // Test retrieving the user by username
+            User retrievedUser = userDAO.doRetrieveByUsername("testUser");
+
+            // Assert that the retrieved user is not null
+            assertNotNull(retrievedUser);
+
+            // Assert that the retrieved user has the expected attributes
+            assertEquals("testUser", retrievedUser.getUsername());
+            assertEquals("Test", retrievedUser.getNome());
+            assertEquals("User", retrievedUser.getCognome());
+            assertEquals("testuser@example.com", retrievedUser.getEmail());
+            assertEquals("1234567890", retrievedUser.getTelefono());
+            assertEquals("Test Address", retrievedUser.getIndirizzo());
+            assertEquals("testpassword", retrievedUser.getPassword());
+            assertFalse(retrievedUser.isAdmin());  // Assuming the test user is not an admin
+
+        } catch (SQLException e) {
+            // If an exception occurs during the test, fail the test
+            fail("Exception occurred: " + e.getMessage());
+        } finally {
+            // Clean up: remove the test user from the database
+            userDAO.deleteUser("testUser");
+        }
+    }
+}
